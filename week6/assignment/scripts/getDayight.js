fb9fc8f7fcde00a6699da02420fb416010d07422
Nickthef <nicolas.franks@bryanuniversity.edu>
2022-09-16 22:14:52 -0400

    weather APP API

--- week6/assignment/scripts/getDayight.js
+++ week6/assignment/scripts/getDayight.js
@@ -1,2 +1,10 @@
 // This script should return the results of a function that use data from the api to determine whether it 
 // is daylight in the specified city. It should return "blue" if daylight, and "black" if night time.
+export default function getDaylight(sunrise, sunset, time) {
+    const weatherDiv = document.querySelector(".weatherWrapper");
+    if (sunrise > time && sunset < time) {
+        weatherDiv.style.backgroundColor = "black";
+    } else if (sunrise < time && sunset > time) {
+        weatherDiv.style.backgroundColor = "blue";
+    }
+}
--- week6/assignment/scripts/weather.js
+++ week6/assignment/scripts/weather.js
@@ -13,3 +13,34 @@
 // create a function that writes the temperature (using local units), humidity, and conditions
 // this function should also change the background color of the weather app to blue during the daylight
 // hours at the specified city
+
+import convertTemp from "./convertTemp.js";
+import getDaylight from "./getDayight.js";
+
+const button = document.getElementById("goBttn");
+button.addEventListener("click", async function () {
+    const input = document.getElementById("city");
+    const response = await getData(input.value);
+    displayData(response) 
+});
+
+async function getData (cityname) {
+    const key = "8e2b888c8739bdea6ecec632123a2417";
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${key}`);
+    const data = await response.json();
+    return data;
+}
+
+function displayData(response) {
+    console.log(response)
+
+    const temp = convertTemp(response.sys.country , response.main.temp);
+    getDaylight(response.sys.sunrise , response.sys.sunset , response.dt);
+
+    document.getElementById("conditionsData").textContent = response.weather;
+    document.getElementById("tempData").textContent = temp;
+    document.getElementById("humidData").textContent = response.main.humidity;
+
+
+}
+
