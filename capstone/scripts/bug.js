437da9cb5f01aab7c9125893b51392579b21454b
Nickthef <nicolas.franks@bryanuniversity.edu>
2022-09-24 22:48:31 -0400

    bug capstone

--- capstone/scripts/bug.js
+++ capstone/scripts/bug.js
@@ -1,28 +1,80 @@
 // Create a variable of the right kind and in the right place such that each new bug that is added can increment that number
 
 class Bug {
-    constructor() {
+    constructor(reportedBy, system, subSystem, bugDesc) {
+
+        this.reportedBy = reportedBy;
+        this.system = system;
+        this.subSystem = subSystem;
+        this.bugDesc = bugDesc;
+
         // This constructor should be set up to accept the four user-input values from index.html: 
         // reportedBy, system, subSystem, and bugDesc
     }
 
     addBug() {
+
+        let listWrapper = document.getElementById("listWrapper");
+        let bugInfo = document.createElement("div");
+        listWrapper.append(bugInfo);
+
+        let myP1 = document.createElement("p");
+        myP1.textContent = `Reported By: ${this.reportedBy}`;
+
+        let myP2 = document.createElement("p");
+        myP2.textContent = `System: ${this.system} / ${this.subSystem}`;
+
+        let myP3 = document.createElement("p");
+        myP3.textContent = `Description: ${this.bugDesc}`;
+
+        let btn1 = document.createElement("button");
+        let btn2 = document.createElement("button");
+
+        btn1.textContent = "Resolve";
+        btn2.textContent = "Delete";
+
+        btn1.addEventListener("click", this.resolveBug);
+        btn2.addEventListener("click", this.deleteBug);
+
+        bugInfo.append(myP1, myP2, myP3, btn1, btn2);
+
         // Create a div element that displays the bug information input by the user within the "listWrapper" DOM element. 
         // It should also contain buttons whose onClick events will call the deleteBug() and resolveBug() methods (see below). 
 
     }
 
-    deleteBug() {
+    deleteBug(e) {
+        e.target.parentNode.remove();
         // Create code that will remove the appropriate bug from the DOM. 
         // You may need to Google how to remove an element from the DOM.
     }
 
-    resolveBug() {
+    resolveBug(e) { 
+        e.target.parentNode.style.backgroundColor = "Grey";
         // Create code that changes the appropriate bug report to a darker color
     }
 }
 
-function reportBug() {
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", reportBug);
+
+
+function reportBug(e) {
+
+    e.preventDefault();
+
+    let reportedBy = document.getElementById("reportedBy");
+    let system = document.getElementById("system");
+    let subSystem = document.getElementById("subSystem");
+    let bugDesc = document.getElementById("bugDesc");
+
+    let bug = new Bug(reportedBy.value, system.options[system.selectedIndex].text, subSystem.options[subSystem.selectedIndex].text, bugDesc.value);
+    
+
+    console.log(system.options[system.selectedIndex].text)
+
+
+    bug.addBug()
     // Create code that instantiates the Bug class with the data input by the 
     // user in the index.html form. Then call the method to add the new bug report.
 }
--- capstone/scripts/data.js
+++ capstone/scripts/data.js
@@ -1,7 +1,22 @@
 // Call the "getSystems()" function in such a way that when the page loads, the "system" select element displays the three sytems 
 // whose parentID is zero.
+const system = document.getElementById("system");
+getSystems(0, system);
+function getSystems(parentID, element) {
 
-function getSystems() {
+    fetch("data.json")
+    .then((Response) => Response.json())
+    .then((data) => {
+        let filter = data.systems.filter(function (obj) {
+            if (obj.parentID == parentID) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        populateDD(filter, element);
+    })
+    .catch((error) => alert(error));
     // This function will accept two arguments (see week three): the parentID and the DOM element that will ultimately receive the data. 
     // Code this function as an asynchronous operation that will fetch data from the data.json file (see week three).
     // After receiving the JSON data, the asynchronous promise should use a higher order array method (see week two) to return only 
@@ -12,15 +27,31 @@ function getSystems() {
     // instead, and may need to be converted to an integer. 
 }
 
-function populateDD() {
+function populateDD(filter, element) {
+    element.textContent = "";
+    let option = document.createElement("option");
+        element.append(option);
+        option.textContent = "Select An Item";
+        
+    for (let i = 0; i < filter.length; i++) {
+        let option = document.createElement("option");
+        element.append(option);
+        option.textContent = filter[i].sysName;
+        option.value = filter[i].sysID;
+    }
     // This function receives the array and DOM element from the "getSystems()" function (above). This function should fill the 
     // appropriate DOM element with options from which the user can select. However, since that DOM element has an "onChange" event, this
     // function first needs to add an option that says "Select an Item". Then use a looping mechanism (week one) to create the rest of  
     // the select element's options using the sysName and sysID from the passed in array (see week five).
 }
 
-document.querySelector('#system').addEventListener(
+document.querySelector('#system').addEventListener("change", function(event) {
+    let element = document.getElementById("subSystem");
+    let parentID = Number(event.target.value);
+
+    getSystems(parentID, element);
+});
+
     // This eventListener responds to a change to the "system" select element by passing the selected value from the "system" element 
     // to the "getSystems()" function along with the "subSystem" DOM element so that the second drop-down list is populated with the 
     // appropriate sub-systems from the data.json file.
-)
